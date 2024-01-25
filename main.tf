e9c4c8326317704193f007f79b2f9bda4468842b
quadribello <quadribello41@gmail.com>
2024-01-25 13:34:17 -0500

    chore: install and configure calico CNI on control plane

--- main.tf
+++ main.tf
@@ -23,10 +23,42 @@ resource "aws_eks_cluster" "cluster" {
 
   vpc_config {
     subnet_ids = flatten([
-      [for i in range(length(module.eks-vpc.public)): module.eks-vpc.public[i]],
-      [for i in range(length(module.eks-vpc.private)): module.eks-vpc.private[i]]
+      [for i in range(length(module.eks-vpc.public)) : module.eks-vpc.public[i]],
+      [for i in range(length(module.eks-vpc.private)) : module.eks-vpc.private[i]]
     ])
   }
 }
 
+resource "null_resource" "eks_kubeconfig_updater" {
+  depends_on = [aws_eks_cluster.cluster]
+
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --region ${var.region} --name ${var.project}"
+  }
+}
+
+resource "null_resource" "rm_aws_node" {
+  depends_on = [null_resource.eks_kubeconfig_updater]
+
+  provisioner "local-exec" {
+    command = "kubectl delete daemonset -n kube-system aws-node"
+  }
+}
+
+resource "null_resource" "install_calico" {
+  depends_on = [null_resource.rm_aws_node]
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/calico-vxlan.yaml"
+  }
+}
+
+#Configure Calico to disable AWS src/dst checks.
+resource "null_resource" "aws_src_dst_checks" {
+  depends_on = [null_resource.install_calico]
+
+  provisioner "local-exec" {
+    command = "kubectl -n kube-system set env daemonset/calico-node FELIX_AWSSRCDSTCHECK=Disable"
+  }
+}
 
